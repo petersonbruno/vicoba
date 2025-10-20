7213286857e06fb8608d77b0ac9231ca625ecb31
petersonbruno <aloyceptrsn@gmail.com>
2025-10-20 08:29:17 +0300

    api client

--- app/lib/apiClient.js
+++ app/lib/apiClient.js
@@ -1,10 +1,23 @@
+// import axios from "axios";
+
+// // apiClient.js
+// const apiClient = axios.create({
+//   // baseURL: "http://localhost:8000/api/", // matches ALLOWED_HOSTS
+//   baseURL: "aloyceptrsn7.pythonanywhere.com", // matches ALLOWED_HOSTS
+//   headers: { "Content-Type": "application/json" },
+// });
+
+// export default apiClient;
+
+
 import axios from "axios";
 
 // apiClient.js
 const apiClient = axios.create({
-  // baseURL: "http://localhost:8000/api/", // matches ALLOWED_HOSTS
-  baseURL: "aloyceptrsn7.pythonanywhere.com", // matches ALLOWED_HOSTS
-  headers: { "Content-Type": "application/json" },
+  baseURL: "https://aloyceptrsn7.pythonanywhere.com/api/", // ✅ Full URL with protocol + correct path
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default apiClient;
